a7e3693566738203e5b983e1be4aaea115409d60
Josh Holder <josh.holder@theguardian.com>
2017-11-02 12:34:42 +0000

    Warming project css

--- src/render.js
+++ src/render.js
@@ -1,3 +1,3 @@
 export async function render() {
-    return "";
+    return `<div class="interactive-key"><p>Osaka's <span style="background-color: #fbedcc;">built up areas</span> house millions. Sea-level rise will flood <span style="background-color: #206a94;color: #fff; ">densely populated</span> and <span style="background-color: #74b3d3; color: #fff;">sparsely populated</span> areas</p></div>`;
 }
