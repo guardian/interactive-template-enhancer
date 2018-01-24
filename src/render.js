eeb760d151a05d6a5611d59c5e0b93fd66b982c5
Josh Holder <josh.holder@theguardian.com>
2018-01-24 15:39:47 +0000

    Update

--- src/render.js
+++ src/render.js
@@ -1,4 +1,4 @@
 export async function render() {
 
-    return `<div class="interactive-key"><p>Millions of people live in the <span style="background-color: #fbedcc;">urban area</span> surrounding Osaka. Sea-level rise will reshape <span style="background-color: #206a94;color: #fff; ">densely</span> and <span style="background-color: #74b3d3; color: #fff;">sparsely</span> populated areas.</p></div>`;
+    return ``;
 }
