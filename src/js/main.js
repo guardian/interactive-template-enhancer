b666c19cb646bc75422a03f09beac31aa2b4089e
Feilding Cage <feilding.cage@gmail.com>
2018-02-06 10:34:45 +0000

    updates header transition

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
- 
+var container = document.createElement('div');
+container.className = 'gv-loop';
+
+var containerInner = document.createElement('div');
+containerInner.className = 'gv-loop-inner';
+
+var el = document.querySelector('header.content__head.content__head--interactive .gs-container');
+el.appendChild(container);
+container.appendChild(containerInner);
