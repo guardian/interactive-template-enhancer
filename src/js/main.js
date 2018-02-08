94429ab548092bb99bb241aafd228c8bed937ede
Feilding Cage <feilding.cage@gmail.com>
2018-02-08 09:10:31 +0000

    mega header fixes for android app :(

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,9 @@ container.className = 'gv-loop';
 var containerInner = document.createElement('div');
 containerInner.className = 'gv-loop-inner';
 
-var el = document.querySelector('header.content__head.content__head--interactive .gs-container, .article__header');
+var el = document.querySelector('header.content__head.content__head--interactive .gs-container, .android .article__header');
 
-el.appendChild(container);
-container.appendChild(containerInner);
+if(el){
+  el.appendChild(container);
+  container.appendChild(containerInner);
+}
