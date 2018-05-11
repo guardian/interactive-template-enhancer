67d8531b1c9e1c37105006781380609cf3ba45e0
Feilding Cage <feilding.cage@gmail.com>
2018-05-11 10:20:37 +0100

    first try

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
- 
+var container = document.createElement('div');
+container.className = 'gv-loop';
+
+var containerInner = document.createElement('div');
+containerInner.className = 'gv-loop-inner';
+
+var el = document.querySelector('header.content__head.content__head--interactive .gs-container, .android .article__header');
+console.log(el)
+if(el){
+  el.appendChild(container);
+  container.appendChild(containerInner);
+}
